52c66082061457d55e7605b42bfe44c7e08642e0
felixcca@gmail.com <felixcca@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-26 20:11:06 +0000

    Got a dumb tower. yay

---
+++ trunk/Unity/Assets/TestBeds/Félix/Bullet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    public Collider target;
+    public int damage;
+    public float speed;
+
+	public void Start()
+    { }
+
+    public void OnTriggerEnter()
+    {
+        Destroy(gameObject);
+    }
+
+	public void Update()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Vector3 delta = direction * speed * Time.deltaTime;
+        transform.position += delta;
+	}
+}
