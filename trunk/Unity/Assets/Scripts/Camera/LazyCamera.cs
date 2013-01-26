42f2e492945df1bbb37fb629d93dfb04a2ab1243
ashoulson@gmail.com <ashoulson@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-26 22:16:40 +0000

    Adding LazyCamera and SongPlayer prefabs.

---
+++ trunk/Unity/Assets/Scripts/Camera/LazyCamera.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class LazyCamera : MonoBehaviour 
+{
+    public Transform Target = null;
+    public float DistanceThreshold = 0.0f;
+
+    private Vector3 CalcDelta()
+    {
+        Vector3 tarPos = Target.transform.position;
+        Vector3 curPos = transform.position;
+
+        Vector3 tarPosPlanar = new Vector3(tarPos.x, 0.0f, tarPos.z);
+        Vector3 curPosPlanar = new Vector3(curPos.x, 0.0f, curPos.z);
+
+        return curPosPlanar - tarPosPlanar;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Vector3 delta = CalcDelta();
+        float deltaDist = delta.magnitude;
+
+        Vector3 deltaCapped = delta.normalized * (deltaDist - DistanceThreshold);
+        transform.position -= deltaCapped * Time.fixedDeltaTime * (deltaDist);
+	}
+}
