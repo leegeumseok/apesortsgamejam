2214a169624116ab5db91ff5086f5b52706929b6
felixcca <felixcca@gmail.com>
2013-01-26 20:11:06 +0000

    Got a dumb tower. yay

---
+++ Unity/Assets/TestBeds/Félix/DumbEnemy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class DumbEnemy : MonoBehaviour {
+
+    const float speed = 0.75f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+	}
+}
---
+++ Unity/Assets/TestBeds/Félix/TowerController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerController : MonoBehaviour {
+
+    private List<Collider> mTargets = new List<Collider>();
+    private float timeBeforeNextAttack;
+
+    public float attacksPerSecond;
+    public int bulletDamage;
+    public float bulletSpeed;
+    public Transform bulletTemplate;
+
+	public void Start()
+    {
+        timeBeforeNextAttack = attacksPerSecond;
+        if (bulletDamage == 0)
+            Debug.LogError("Bullets will not do any damage");
+        if (bulletSpeed == 0)
+            Debug.LogError("Bullets have a zero speed");
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        mTargets.Add(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        mTargets.Remove(other);
+    }
+	
+	// Update is called once per frame
+	public void Update()
+    {
+        timeBeforeNextAttack -= Time.deltaTime;
+        if (timeBeforeNextAttack <= 0)
+        {
+            Collider target = FindClosestTarget();
+            if (target != null)
+            {
+                Transform bullet = (Transform)Instantiate(bulletTemplate);
+                Bullet component = bullet.GetComponent<Bullet>();
+                component.target = target;
+                component.damage = bulletDamage;
+                component.speed = bulletSpeed;
+
+                timeBeforeNextAttack = 1 / attacksPerSecond;
+            }
+        }
+	}
+
+    private Collider FindClosestTarget()
+    {
+        if (mTargets.Count == 0)
+            return null;
+
+        Collider closest = mTargets[0];
+        float closestMagnitude = closest.ClosestPointOnBounds(transform.position).sqrMagnitude;
+        for (int i = 1; i < mTargets.Count; i++)
+        {
+            Collider target = mTargets[i];
+            float magnitude = target.ClosestPointOnBounds(transform.position).sqrMagnitude;
+            if (magnitude < closestMagnitude)
+            {
+                closest = target;
+                closestMagnitude = magnitude;
+            }
+        }
+        return closest;
+    }
+}
