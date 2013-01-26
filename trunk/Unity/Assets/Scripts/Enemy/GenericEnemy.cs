b30bd61d92d5d1a852d095d9d4d87dda1753b288
ashoulson@gmail.com <ashoulson@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-26 22:44:30 +0000

    Adding Generic Enemy

---
+++ trunk/Unity/Assets/Scripts/Enemy/GenericEnemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class GenericEnemy : BeatReceiver 
+{
+    public virtual void OnSpawn() { }
+    public virtual void OnHit() { }
+    public virtual void OnGrabbed() { }
+    public virtual void OnDamaged() { }
+    public virtual void OnDestroyed() { }
+
+    // Probably won't use this directly, but I'll add it anyway
+    public override void OnBeat() { }
+}
