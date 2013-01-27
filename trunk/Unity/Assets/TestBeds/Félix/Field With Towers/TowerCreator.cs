b8dddf9145323836b6707bf2748dd234d97a075f
felixcca@gmail.com <felixcca@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 00:56:27 +0000

    Took the tower creation behavior to a different script

---
+++ trunk/Unity/Assets/TestBeds/Félix/Field With Towers/TowerCreator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class TowerCreator : MonoBehaviour {
+
+    // Towers
+    public Transform spawnLocation;
+    public Transform towerTemplate;
+    public int resources = 0;
+    public int towerResourceCost = 10;
+    public int maxResources = 15;
+	
+	// Update is called once per frame
+    void Update()
+    {
+        // Place a tower?
+        if (Input.GetKeyDown("q") && resources > towerResourceCost)
+        {
+            Transform tower = (Transform)Instantiate(towerTemplate, spawnLocation.position, Quaternion.identity);
+            TowerController controller = tower.GetComponent<TowerController>();
+            controller.bulletSpeed = 4;
+
+            resources -= towerResourceCost;
+        }
+	}
+
+    void GatherResources(int resources)
+    {
+        this.resources += resources;
+        if (this.resources > maxResources)
+            this.resources = maxResources;
+    }
+}
