5f3324f48766ea583b0a788ee13707b56731793f
ashoulson <ashoulson@gmail.com>
2013-01-27 04:43:25 +0000

    Added event on destroy to basic enemy. And googly eyes.

--- Unity/Assets/Scripts/Enemy/GenericEnemy.cs
+++ Unity/Assets/Scripts/Enemy/GenericEnemy.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class GenericEnemy : BeatReceiver 
 {
+    public delegate void OnDestroyedNotify();
+    public event OnDestroyedNotify notifyOnDestroyed;
+
     public virtual void OnSpawn() { }
     public virtual void OnHit() { }
     public virtual void OnGrabbed() { }
     public virtual void OnDamaged(int damage) { }
-    public virtual void OnDestroyed() { }
+
+    public virtual void OnDestroyed() 
+    {
+        if (notifyOnDestroyed != null)
+            notifyOnDestroyed.Invoke();
+    }
 
     // Probably won't use this directly, but I'll add it anyway
     public override void OnBeat() { }
