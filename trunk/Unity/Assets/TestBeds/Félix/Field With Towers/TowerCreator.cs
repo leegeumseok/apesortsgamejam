2b96cdb3d3a81c09090fa528464ba98124afe483
felixcca@gmail.com <felixcca@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 17:20:34 +0000

    Towers are now antibody towers. They don't animate though.

--- trunk/Unity/Assets/TestBeds/Félix/Field With Towers/TowerCreator.cs
+++ trunk/Unity/Assets/TestBeds/Félix/Field With Towers/TowerCreator.cs
@@ -9,6 +9,14 @@ public class TowerCreator : MonoBehaviour {
     public int resources = 0;
     public int towerResourceCost = 10;
     public int maxResources = 15;
+
+    public int bulletSpeed = 25;
+    public int bulletDamage = 100;
+    public int bulletForce = 60;
+
+    public float attackRange = 5;
+    public float attacksPerSecond = 1.75f;
+    public int ammunition = 50;
 	
 	// Update is called once per frame
     void Update()
@@ -18,11 +26,12 @@ public class TowerCreator : MonoBehaviour {
         {
             Transform tower = (Transform)Instantiate(towerTemplate, spawnLocation.position, Quaternion.identity);
             GenericTower controller = tower.GetComponent<GenericTower>();
-            controller.bulletSpeed = 10;
-            controller.bulletDamage = 100;
-            controller.bulletForce = 60;
-            controller.attacksPerSecond = 1.75f;
-            controller.ammunition = 50;
+            controller.attackRange = attackRange;
+            controller.bulletSpeed = bulletSpeed;
+            controller.bulletDamage = bulletDamage;
+            controller.bulletForce = bulletForce;
+            controller.attacksPerSecond = attacksPerSecond;
+            controller.ammunition = ammunition;
 
             resources -= towerResourceCost;
         }
--- trunk/Unity/Assets/TestBeds/Félix/GenericTower.cs
+++ trunk/Unity/Assets/TestBeds/Félix/GenericTower.cs
@@ -13,8 +13,10 @@ public class GenericTower : MonoBehaviour {
     public float bulletSpeed;
     public float bulletForce;
     public int ammunition = int.MaxValue;
-    public Transform bulletTemplate;
+    public GameObject bulletTemplate;
+    public Transform bulletSpawnPoint;
     public LayerMask targetLayer;
+    public string fireAnimation;
 
     public event Action notifyOnDestroyed;
 
@@ -25,11 +27,18 @@ public class GenericTower : MonoBehaviour {
             onDestroyed();
     }
 
+    public virtual void OnAttack(Collider target)
+    {
+
+    }
+
 	public void Start()
     {
         timeBeforeNextAttack = 1 / attacksPerSecond;
+
         if (bulletDamage == 0)
             Debug.LogError("Bullets will not do any damage");
+
         if (bulletSpeed == 0)
             Debug.LogError("Bullets have a zero speed");
 	}
@@ -45,10 +54,6 @@ public class GenericTower : MonoBehaviour {
             Collider target = FindClosestTarget(collidedWith);
             if (target != null)
             {
-                Vector3 lookAt = target.transform.position;
-                lookAt.y = transform.position.y;
-                transform.LookAt(lookAt);
-
                 if (timeBeforeNextAttack <= 0)
                 {
                     Attack(target);
@@ -65,14 +70,16 @@ public class GenericTower : MonoBehaviour {
     private void Attack(Collider target)
     {
         ammunition--;
-        Transform bullet = (Transform)Instantiate(bulletTemplate);
-        bullet.position = transform.position;
+        GameObject bullet = (GameObject)Instantiate(bulletTemplate);
+        bullet.transform.position = bulletSpawnPoint.transform.position;
 
         HomingBullet component = bullet.GetComponent<HomingBullet>();
         component.target = target;
         component.damage = bulletDamage;
         component.speed = bulletSpeed;
         component.force = bulletForce;
+
+        OnAttack(target);
     }
 
     private Collider FindClosestTarget(Collider[] targets)
