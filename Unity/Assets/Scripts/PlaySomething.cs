d8e57c639df3b5e61c94b2c3bdd28d6f096069e8
ashoulson <ashoulson@gmail.com>
2013-01-27 03:36:45 +0000

    PlaySomething can now take an input path.

--- Unity/Assets/Scripts/PlaySomething.cs
+++ Unity/Assets/Scripts/PlaySomething.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class PlaySomething : MonoBehaviour 
 {
+    public string pathToSong = "C:\\Users\\Alex\\Downloads\\Hotline Miami Soundtrack -  Perturbator.mp3";
+
 	void Update () 
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            SongPlayer.Instance.PlaySong("C:\\Users\\Alex\\Downloads\\Hotline Miami Soundtrack -  Perturbator.mp3");
+            SongPlayer.Instance.PlaySong(pathToSong);
         }
 	}
 }
