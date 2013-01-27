60127a9d67c6e0b2f3158d9ad6fe78a4a7930797
fortytwo.adam <fortytwo.adam@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 19:08:10 +0000

    added respawn countdown -Adam

--- Unity/Assets/Main Menu Assets/Scripts/GUI/GameStatsUI.cs
+++ Unity/Assets/Main Menu Assets/Scripts/GUI/GameStatsUI.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameStatsUI : MonoBehaviour {
 	public GUISkin customSkin;
 	public int time, points, heartHealth, heartHealthMax, pulsePoints, pulsePointsMax, resources;
+	public float respawnTimerCount;
 	
 	// Use this for initialization
 	void Start () {
@@ -31,5 +32,11 @@ public class GameStatsUI : MonoBehaviour {
 			GUI.Label(new Rect(Screen.width - 100f, Screen.height*0.5f,100f,50f), "Pulse: " + pulsePoints + "/" + pulsePointsMax);
 		}
 		GUI.Label(new Rect(Screen.width - 100f, Screen.height*0.75f,100f,50f), "Resources: " + resources);
+		
+		//RESPAWN
+		if (respawnTimerCount > 0f){
+			GUI.Label (new Rect(Screen.width/2 - 50f, Screen.height/2 - 50f,100f,100f), "Respawn in: " + (((int) respawnTimerCount) + 1), "PointsLabel");
+			respawnTimerCount -= Time.deltaTime;
+		}
 	}
 }
