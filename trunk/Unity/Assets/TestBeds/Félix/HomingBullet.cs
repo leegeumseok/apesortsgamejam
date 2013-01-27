9946d0d2d19f78b7dbf68473e5bf216734ecec38
felixcca@gmail.com <felixcca@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 03:37:06 +0000

    Now with explosions

--- trunk/Unity/Assets/TestBeds/Félix/HomingBullet.cs
+++ trunk/Unity/Assets/TestBeds/Félix/HomingBullet.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class HomingBullet : MonoBehaviour
 {
+    public GameObject explosion;
     public Collider target;
     public int damage;
     public float speed;
     public float force;
 
 	public void Start()
-    { }
+    {
+        Rigidbody body = target.rigidbody;
+        if (body != null)
+        {
+            body.WakeUp();
+        }
+    }
 
     public void OnTriggerEnter(Collider collider)
     {
@@ -17,8 +24,10 @@ public class HomingBullet : MonoBehaviour
         if (body != null)
         {
             Vector3 direction = (collider.transform.position - transform.position).normalized;
-            body.AddForce(direction * force);
+            body.AddForce(direction * force, ForceMode.Impulse);
         }
+
+        Instantiate(explosion, collider.transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 
