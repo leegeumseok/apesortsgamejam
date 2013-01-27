27767f87cb2563a0277b5c1aee6cf45f9860f261
ashoulson <ashoulson@gmail.com>
2013-01-27 01:34:47 +0000

    Boids won't collide with a dead player, as long as it's set in the Player class.

---
+++ Unity/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+    private static Player _instance = null;
+    public static Player Instance
+    {
+        get
+        {
+            return _instance;
+        }
+
+        private set
+        {
+            if (_instance != null)
+                Debug.LogError("Multiple Player singletons");
+            _instance = value;
+        }
+    }
+
+    public bool Alive = true;
+}
