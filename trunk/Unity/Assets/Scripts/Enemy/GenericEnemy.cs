d9ea08d365f242d00f172433b6f962d701d1a29d
felixcca@gmail.com <felixcca@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 05:52:27 +0000

    Moved the hit points logic

--- trunk/Unity/Assets/Scripts/Enemy/GenericEnemy.cs
+++ trunk/Unity/Assets/Scripts/Enemy/GenericEnemy.cs
@@ -4,16 +4,26 @@ using System.Collections;
 public class GenericEnemy : BeatReceiver
 {
     public delegate void OnDestroyedNotify();
-
-	public int healthPoints = 100;
+    public int healthPoints = 100;
     public int resourceValue = 1;
+
     public event OnDestroyedNotify notifyOnDestroyed;
 
     public virtual void OnSpawn() { }
     public virtual void OnHit() { }
     public virtual void OnGrabbed() { }
     public virtual void OnReleased() { }
-    public virtual void OnDamaged(int damage) { }
+
+    public virtual void OnDamaged(int damage)
+    {
+        // see subclasses for actual death behavior
+        healthPoints -= damage;
+        if (healthPoints <= 0)
+        {
+            Boids boid = this.GetComponent<Boids>();
+            boid.AvoidanceRule = boid.GoalSeekingRule = false;
+        }
+    }
 
     public virtual void OnDestroyed() 
     {
