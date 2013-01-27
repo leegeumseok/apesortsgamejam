6882966f546e309cd9cf06ae06689dfc5121b27e
kuzymanic@gmail.com <kuzymanic@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 06:56:16 +0000

    player pulse, heartbeat pulse, new material on pulse

---
+++ trunk/Unity/Assets/Scripts/Gfx/HeartBeatCopy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeartBeatCopy : BeatReceiver 
+{
+	public GameObject mHeartPulse;
+    public float currentScale = 16.0f;
+    public float minScale = 16.0f;
+    public float maxScale = 22.0f;
+
+    void Start()
+    {
+        BeatManager.Instance.RegisterReceiver(this, 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentScale > minScale)
+            currentScale *= 0.97f;
+        if (currentScale < minScale)
+            currentScale = minScale;
+
+        transform.localScale = new Vector3(
+            currentScale, currentScale, currentScale);
+    }
+
+    public override void OnBeat()
+    {
+        this.currentScale = maxScale;
+		GameObject clone;
+		clone = Instantiate(mHeartPulse, transform.position, Quaternion.identity) as GameObject;
+		clone.GetComponent<Pulse>().mMaxRadius = 7f;
+    }
+}
--- trunk/Unity/Assets/Scripts/Gfx/Heartbeat.cs
+++ trunk/Unity/Assets/Scripts/Gfx/Heartbeat.cs
@@ -27,5 +27,6 @@ public class Heartbeat : BeatReceiver
     public override void OnBeat()
     {
         this.currentScale = maxScale;
+
     }
 }
