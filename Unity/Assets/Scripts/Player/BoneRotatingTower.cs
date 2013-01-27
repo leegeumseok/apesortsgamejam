3257cd9e1ea60b8f4d3a2936e1112d1c800a9ff8
felixcca <felixcca@gmail.com>
2013-01-27 17:20:34 +0000

    Towers are now antibody towers. They don't animate though.

---
+++ Unity/Assets/Scripts/Player/BoneRotatingTower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class BoneRotatingTower : GenericTower
+    {
+        public override void OnAttack(UnityEngine.Collider target)
+        {
+            return;
+            Vector3 lookAt = target.transform.position;
+            lookAt.y = transform.position.y;
+            Transform joint = transform.FindChild("joint1");
+            joint.LookAt(lookAt);
+        }
+    }
+}
