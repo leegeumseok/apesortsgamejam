3a597c7e3e590a735f4925e33f19f325ea8c1ee3
felixcca@gmail.com <felixcca@gmail.com@0457b838-05c3-e09c-27a6-d3467197a575>
2013-01-27 17:44:45 +0000

    Added score management

---
+++ trunk/Unity/Assets/Scripts/Player/PlayerScore.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+
+    public int currentScore;
+
+	// Use this for initialization
+	void Start () {
+        BoidSpawnManager.Instance.notifyBoidCreated += OnEnemyCreated;
+	}
+
+    private void OnEnemyCreated(BoidController obj)
+    {
+        GenericEnemy enemy = obj.GetComponent<GenericEnemy>();
+        if (enemy != null)
+        {
+            enemy.notifyOnDestroyed += OnEnemyKilled;
+        }
+    }
+
+    private void OnEnemyKilled(GenericEnemy enemy)
+    {
+        currentScore += enemy.killPoints;
+        enemy.notifyOnDestroyed -= OnEnemyKilled;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
